9ba094af8a3ec988507ee8c5aaeb43c223eac330
damzymichael <105460672+damzymichael@users.noreply.github.com>
2024-02-05 11:58:48 +0100

    nav error fix try

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import {useReducer, useEffect} from 'react';
+import {useReducer, useEffect, useRef} from 'react';
 import {motion, AnimatePresence} from 'framer-motion';
 import './hamburger.css';
 
@@ -22,6 +22,7 @@ const variants = (delay = 0.5) => ({
 });
 
 const Header = () => {
+  const headerRef = useRef<HTMLDivElement>(null);
   const nav = [
     'Home',
     'About me',
@@ -32,20 +33,23 @@ const Header = () => {
   ];
   const [navOpen, toggleNavOpen] = useReducer(state => !state, false);
   useEffect(() => {
+    //resize event listener
+    // window.addEventListener('resize', () => {
+    //   if (window.innerHeight < 768 && navOpen) toggleNavOpen();
+    // });
     //Add click away listener to close the navbar
     function closeDropdown() {
-      //!Must be set to close
-      toggleNavOpen();
-      document.removeEventListener('click', closeDropdown);
+      //!function must close the navbar
+      if (navOpen) {
+        toggleNavOpen();
+        document.removeEventListener('click', closeDropdown);
+      }
     }
     if (navOpen) document.addEventListener('click', closeDropdown);
   }, [navOpen]);
   return (
     <motion.header
-      className={
-        'md:py-3 w-full fixed top-0 left-1/2 -translate-x-1/2 z-20 bg-gray-800 ' +
-        (navOpen ? 'rounded-b-2xl' : '')
-      }
+      className='md:py-3 w-full fixed top-0 left-1/2 -translate-x-1/2 z-20 bg-gray-800'
       variants={{
         open: {backgroundColor: 'rgb(147 51 234)'},
         closed: {backgroundColor: 'rgb(31 41 55)'}
@@ -53,48 +57,52 @@ const Header = () => {
       animate={navOpen ? 'open' : 'closed'}
       onClick={e => e.stopPropagation()}
     >
-      {/* Desktop nav  */}
-      <nav className='w-max hidden md:flex gap-4 mx-auto'>
-        {nav.map(nav => (
-          <a href='#' key={nav}>
-            {nav}
-          </a>
-        ))}
-      </nav>
+      <div ref={headerRef}>
+        {/* Desktop nav  */}
+        <nav className='w-max hidden md:flex gap-4 mx-auto'>
+          {nav.map(nav => (
+            <a href='#' key={nav}>
+              {nav}
+            </a>
+          ))}
+        </nav>
 
-      {/* Navigation button  */}
-      <button
-        className={'flex md:hidden hamburger ' + (navOpen ? 'open' : '')}
-        onClick={toggleNavOpen}
-      >
-        {Array.from({length: 3}).map((_, i) => (
-          <span key={i}></span>
-        ))}
-      </button>
+        {/* Navigation button  */}
+        <button
+          className={'flex md:hidden hamburger ' + (navOpen ? 'open' : '')}
+          onClick={toggleNavOpen}
+        >
+          {Array.from({length: 3}).map((_, i) => (
+            <span key={i}></span>
+          ))}
+        </button>
 
-      {/* Mobile nav  */}
-      <AnimatePresence>
-        {navOpen && (
-          <motion.ul
-            className='bg-purple-600 p-5 pt-0 flex flex-col gap-3 overflow-hidden rounded-b-2xl'
-            initial={{height: 0, opacity: 0}}
-            animate={{height: 'auto', opacity: 1}}
-            exit={{height: 0, opacity: 0, transition: {delay: 0.6}}}
-            onClick={toggleNavOpen}
-          >
-            {nav.map((_, i) => (
-              <motion.li
-                key={i}
-                initial={variants().closed}
-                animate={variants(i * 0.2).open}
-                exit={variants(1 / (i + 1)).closed}
-              >
-                <a href='#'>{_}</a>
-              </motion.li>
-            ))}
-          </motion.ul>
-        )}
-      </AnimatePresence>
+        {/* Mobile nav  */}
+        <AnimatePresence>
+          {navOpen && (
+            <motion.ul
+              className={`bg-purple-600 w-full fixed left-0 top-${
+                (headerRef.current?.offsetHeight as number) - 2
+              } p-5 pt-2 md:hidden flex flex-col gap-3 overflow-hidden rounded-b-2xl`}
+              initial={{height: 0, opacity: 0}}
+              animate={{height: 'auto', opacity: 1}}
+              exit={{height: 0, opacity: 0, transition: {delay: 0.6}}}
+              onClick={toggleNavOpen}
+            >
+              {nav.map((_, i) => (
+                <motion.li
+                  key={i}
+                  initial={variants().closed}
+                  animate={variants(i * 0.2).open}
+                  exit={variants(1 / (i + 1)).closed}
+                >
+                  <a href='#'>{_}</a>
+                </motion.li>
+              ))}
+            </motion.ul>
+          )}
+        </AnimatePresence>
+      </div>
     </motion.header>
   );
 };
