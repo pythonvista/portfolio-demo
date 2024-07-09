7abf91b665571ab04953751aa51d275990c83b60
damzymichael <105460672+damzymichael@users.noreply.github.com>
2024-07-09 11:59:14 +0100

    Posthog react

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import posthog from 'posthog-js';
+import {PostHogProvider} from 'posthog-js/react';
 import './index.css';
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only'
+});
 
 //Production
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
 
 //Development
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+// ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
